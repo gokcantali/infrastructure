2e60ceedaba90fa0cddc4994f484106526ff5341
Matthias Osswald <matthias@busykoala.io>
2024-03-20 07:46:58 +0100

    Fix connectivity issue with node join and cilium.

--- install-nodes.sh
+++ install-nodes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-NODE_IPS=(10.0.2.201)
+NODE_IPS=(10.0.2.9)
 
 for NODE_IP in "${NODE_IPS[@]}"; do
     TOKEN=$(ssh ubuntu@bachelor "sudo cat /var/lib/rancher/k3s/server/node-token")
-    ssh ubuntu@bachelor "ssh ubuntu@${NODE_IP} \"curl -sfL https://get.k3s.io | K3S_URL=https://10.0.0.209:6443 K3S_TOKEN='${TOKEN}' sh -\""
+    ssh ubuntu@bachelor "ssh ubuntu@${NODE_IP} \"curl -sfL https://get.k3s.io | K3S_URL=https://160.85.252.183:6443 K3S_TOKEN='${TOKEN}' sh -\""
 done
